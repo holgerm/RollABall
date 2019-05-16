6f1bae5b816805d191ef3468eef295c827c546f6
questmill <git@quest-mill.com>
2019-05-16 05:41:07 +0200

    Haptic Feedback by Vibration

---
+++ Assets/_Completed-Game/Scripts/HapticFeedback.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HapticFeedback : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        // ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            Handheld.Vibrate();
+        }
+    }
+}
