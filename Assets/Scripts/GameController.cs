df7f9d1bae55440f0f27282c37844e19e8b05f15
questmill <git@quest-mill.com>
2019-06-05 17:19:33 +0200

    reactivate all pickups at restart (and start)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,7 +7,9 @@ public class GameController : MonoBehaviour
 {
     public Text countText;
     public Text winText;
-    public SettingsController settings;
+
+    public static GameController Instance;
+    public GameObject pickups;
 
     private int count;
 
@@ -22,6 +24,34 @@ public class GameController : MonoBehaviour
 
         // Set the text property of our Win Text UI to an empty string, making the 'You Win' (game over message) blank
         winText.text = "";
+
+        Instance = this;
+
+    }
+
+    public void StartGame()
+    {
+        count = 0;
+        winText.text = "";
+        GetComponent<Canvas>().gameObject.SetActive(false);
+
+        Transform[] pickupTs = (Transform[])pickups.GetComponentsInChildren<Transform>(true);
+        for (int i = 0; i < pickupTs.Length; i++)
+        {
+            Transform pickup = pickupTs[i];
+            pickup.gameObject.SetActive(true);
+        }
+
+    }
+
+    public void Point()
+    {
+        // Add one to the score variable 'count'
+        count = count + 1;
+
+        // Run the 'SetCountText()' function (see below)
+        SetCountAndWinText();
+        Handheld.Vibrate();
     }
 
     // Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
@@ -35,32 +65,15 @@ public class GameController : MonoBehaviour
         {
             // Set the text value of our 'winText'
             winText.text = "You Win!";
-            settings.GetComponent<Canvas>().gameObject.SetActive(true);
+            GetComponent<Canvas>().gameObject.SetActive(true);
         }
     }
 
     // When this game object intersects a collider with 'is trigger' checked, 
     // store a reference to that collider in a variable named 'other'..
-    void OnTriggerEnter(Collider other)
-    {
-        // ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
-        if (other.gameObject.CompareTag("Pick Up"))
-        {
-            // Make the other game object (the pick up) inactive, to make it disappear
-            other.gameObject.SetActive(false);
-
-            // Add one to the score variable 'count'
-            count = count + 1;
-
-            // Run the 'SetCountText()' function (see below)
-            SetCountAndWinText();
-            Handheld.Vibrate();
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
