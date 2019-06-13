90b25e0b3453e7158c89177c915a5c5828b10040
questmill <git@quest-mill.com>
2019-06-13 10:05:08 +0200

    auto map extension

---
+++ Assets/Scripts/MapExtender.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapExtender : MonoBehaviour
+{
+    [SerializeField]int size = 20;
+    [SerializeField] GameObject prefab;
+    [SerializeField] GameObject parent;
+    int triggerPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggerPosition = size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z >= triggerPosition)
+        {
+            GameObject newGroundObject =  Instantiate(prefab,new Vector3(0, 0, triggerPosition + 2*size), Quaternion.identity);
+            newGroundObject.transform.SetParent(parent.transform);
+            triggerPosition += size;
+        }
+    }
+}
